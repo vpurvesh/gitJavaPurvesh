4a76b45d213fa8d3271b397bd046b536cc2a6033
Purvesh <purvesh2201@gmail.com>
2024-05-23 11:29:41 +0530

    This is second commit. we have changed some coding in HelloWorld.java

--- src/javaTutorial/HelloWorld.java
+++ src/javaTutorial/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Learn Java Coding !!!!!!!!!!!!");
 		System.out.println("Purvesh Vijayvargiya");
+		System.out.print("This is to check git commit");
 	}
 
 }
