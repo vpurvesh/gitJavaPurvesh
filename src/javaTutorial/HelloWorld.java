12715e2901cc205951a04e94d214c2d08573c16f
Purvesh <ankur220191@gmail.com>
2024-05-23 11:05:04 +0530

    First Commit .... Inserting HelloWorld Code into git

---
+++ src/javaTutorial/HelloWorld.java
@@ -0,0 +1,9 @@
+package javaTutorial;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Learn Java Coding !!!!!!!!!!!!");
+		System.out.println("Purvesh Vijayvargiya");
+	}
+
+}
